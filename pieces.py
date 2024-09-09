683bb310acc9cd9715d670e0468d11a3e61650a1
tmkeating <twylakeating93@gmail.com>
2024-09-09 16:20:45 -0400

    added moveset property to piece class and other parameters

--- pieces.py
+++ pieces.py
@@ -1,16 +1,20 @@
 import constants
 
+import pygame
 
 diagonal_moves = [(y * x, z * x) for x in range(1, 9) for y in [1, -1] for z in [-1, 1]]
 gold_moves = [(-1, 1), (0, 1), (1, 1), (-1, 0), (1, 0), (0, -1)]
 
 
 class piece():
-    def __init__(self):
-        self.moveset = None
+    def __init__(self, moveset):
+        self._moveset = moveset
         self.promoted = False
         self.owner = None
         self.image = None
+        self.rect = pygame.rect
+        self.sprite = pygame.sprite.Sprite
+        self.game = None
     
     def capture(self):
         self.promoted = False
@@ -22,12 +26,23 @@ class piece():
     def promote(self):
         self.promoted = True
 
-    def get_moves(self):
-        return self.moveset[(self.type, self.promoted)]
+    # getting the values
+    @property
+    def moveset(self):
+        return self._moveset
+
+    # setting the values
+    @moveset.setter
+    def moveset(self, moveset):
+        self._moveset = moveset
+
+    # deleting the values
+    @moveset.deleter
+    def moveset(self):
+        del self._moveset
+
     
 class pawn(piece):
-    def __init__(self):
+    def __init__(self, moveset):
         super.__init__()
-        self.moveset[False] = [(0, 1)]
-        self.moveset[True] = gold_moves
-        self.image
+
