69ca86377a4cce16c0792644b8d9d7845b6cdf26
AriaAugs <nataug62@gmail.com>
2024-09-12 11:09:08 -0400

    pieces and board properly inherit from pygame.sprite.Sprite

--- gui.py
+++ gui.py
@@ -26,10 +26,6 @@ class GameWindow():
         if not pygame.get_init():
             pygame.init()
         # game to display/control
-        # TODO - replace this with the actual game state
-        # the demo game_state is an array of arrays
-        # each sub-array contains the color and Rectangle object
-        # for each rectangle to draw
         self.game_state = game_state
         # keep track of the selected piece
         self.drag_item = None
@@ -53,7 +49,7 @@ class GameWindow():
                         # drag 'r1' if the mouse is over the rectangle
                         self.drag_item = rect
                         self.drag_rollback = rect.copy()
-            if event.button == 3 and self.drag_item != None:
+            if event.button == 3 and self.drag_item is not None:
                 # if the right mouse button is pressed and we're dragging something,
                 # rollback 'r1' to where it was before we started dragging it
                 for idx in range(len(self.game_state)):
@@ -102,35 +98,47 @@ TODO: Remove for final version of code
 """
 class GameState():
     def __init__(self, board_in, pieces_in):
-        if isinstance(board_in, GameBoard):
-            self.board = pygame.sprite.Group([board_in])
-        else:
-            self.board = board_in
-        if isinstance(pieces_in, list):
-            self.pieces = pygame.sprite.Group(pieces_in)
-        else:
-            self.pieces = pieces_in
+        self.board = pygame.sprite.Group(board_in)
+        self.pieces = pygame.sprite.Group(pieces_in)
 
 class GameBoard(pygame.sprite.Sprite):
-    def __init__(self, width, height, color):
-        super().__init__(self)
+    def __init__(self, board_size, color):
+        # Call the parent class (Sprite) constructor
+        # We could use super().__init__(), but this is more explicit
+        # and avoid any issues from super() accidentally getting the wrong class
+        pygame.sprite.Sprite.__init__(self)
         # create sprite stuff
-        self.image = pygame.Surface([width, height])
+        self.image = pygame.Surface(board_size)
         self.image.fill(color)
         self.rect = self.image.get_rect()
         # lists for the board and held pieces
         self.board = [[None for x in range(9)] for y in range(9)]
         self.held_white = []
         self.held_black = []
+        # create an array of collision points to help check for piece placement
+        self.grid = {}
+        grid_rect = pygame.Rect(0, 0, board_size[0]/9, board_size[1]/9)
+        for x in range(9):
+            for y in range(9):
+                grid_rect.topleft = (x*board_size[0]/9, y*board_size[1]/9)
+                self.grid[(x, y)] = grid_rect.center
+
+    def board_collide(self, rect):
+        for key, pos in self.grid.items():
+            if rect.collidepoint(pos):
+                return key
+        return None
 
 class GamePiece(pygame.sprite.Sprite):
-    def __init__(self, width, height, color):
+    def __init__(self, piece_size, color):
         # Call the parent class (Sprite) constructor
-        super().__init__(self)
+        # We could use super().__init__(), but this is more explicit
+        # and avoid any issues from super() accidentally getting the wrong class
+        pygame.sprite.Sprite.__init__(self)
 
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width, height])
+        self.image = pygame.Surface(piece_size)
         self.image.fill(color)
 
         # Fetch the rectangle object that has the dimensions of the image
@@ -138,8 +146,10 @@ class GamePiece(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 if __name__ == '__main__':
-    # create the pieces
-    pieces = pygame.sprite.Group()
-    board = GameBoard(600, 600, 'gray')
-    test_game = GameWindow()
+    pieces = []
+    pieces.append(GamePiece((60, 60), 'red'))
+    pieces.append(GamePiece((60, 60), 'yellow'))
+    board = GameBoard((600, 600), 'gray')
+    game_state = GameState(board, pieces)
+    test_game = GameWindow(game_state)
     test_game.run()
